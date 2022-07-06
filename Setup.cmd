faa795f777c1360e901a2b22e67985f592db8e47
黄元镭 <huangyuanlei@hotmail.com>
2022-07-06 17:16:10 +0800

    202206071716

--- Setup.cmd
+++ Setup.cmd
@@ -2,5 +2,6 @@
 @taskkill /f /im:asim.exe 2>nul
 @taskkill /f /im:asim_service.exe 2>nul
 @xcopy ".\Program Files\AutoSwitchInputMethod" "C:\Program Files\AutoSwitchInputMethod" /E /C /H /I
+@".\AppPackages\ASIM_1.0.1.0_Test\ASIM_1.0.1.0_x64.msix"
 @popd
 pause
