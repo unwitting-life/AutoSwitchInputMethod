de69582e5136cb3133cc5eeaea6d531d52695eb0
老黄 <huangyuanlei@hotmail.com>
2022-07-16 00:12:59 +0800

    202207161212

--- Setup.cmd
+++ Setup.cmd
@@ -1,6 +1,6 @@
 @echo off
 @taskkill /f /im:asim.exe 2>nul
-@taskkill /f /im:asim_service.exe 2>nul
+@taskkill /f /im:asim_core.exe 2>nul
 @xcopy "%~dp0Program Files\AutoSwitchInputMethod" "C:\Program Files\AutoSwitchInputMethod" /E /C /H /I /Y
 @popd
 exit /b
